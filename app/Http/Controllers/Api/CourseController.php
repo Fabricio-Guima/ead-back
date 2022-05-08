f5c592796febbe02003dece2345d01be762433de
Fabricio-Guima <fsgkof@gmail.com>
2022-05-08 19:22:11 -0300

    #camada CourseRepository

--- app/Http/Controllers/Api/CourseController.php
+++ app/Http/Controllers/Api/CourseController.php
@@ -4,22 +4,25 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\CourseResource;
-use App\Models\Course;
+use App\Repositories\CourseRepository;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
-    public function index()
-    {
-        $courses = Course::get();
+    protected $repository;	
+	
+	public function __construct(CourseRepository $courseRepository)
+	{	
+		$this->repository = $courseRepository;
+	}
 
-        return CourseResource::collection($courses) ;
+    public function index()
+    {      
+        return CourseResource::collection($this->repository->getAllCourses());
     }
 
     public function show(String $id)
-    {
-        $course = Course::findOrFail($id);
-        
-        return new CourseResource($course);
+    {     
+        return new CourseResource($this->repository->getCourse($id));
     }
 }
---
+++ app/Repositories/CourseRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Course;
+
+class CourseRepository
+{
+	protected $entity;	
+
+	public function __construct(Course $model)
+	{	
+		$this->entity = $model;
+	}
+
+	public function getAllCourses()
+	{
+		return $this->entity::get();
+	}
+
+	public function getCourse(String $identify)
+	{
+		return $this->entity::find($identify);
+	}
+
+
+}
