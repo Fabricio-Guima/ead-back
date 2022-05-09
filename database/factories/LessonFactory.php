cd81a60b253d32c0a44b163952c23e55e1e8864e
Fabricio-Guima <fsgkof@gmail.com>
2022-05-08 23:46:16 -0300

    #factories de Lesson e Module

---
+++ database/factories/LessonFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Lesson;
+use App\Models\Module;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class LessonFactory extends Factory
+{
+    protected $model = Lesson::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $name = $this->faker->name();
+        return [
+            'module_id' => Module::factory(),
+            'name' => $name,
+            'url' => Str::slug($name),
+            'video' => Str::random(),            
+        ];
+    }
+}
